b805591694ae44677bf9da53a71ee3a04f356f39
Bhavik Sachdev <b.sachdev1904@gmail.com>
2023-10-02 03:16:30 +0530

    Added solver.py Declares functions required for implementing a basic solver.

---
+++ src/solver.py
@@ -0,0 +1,13 @@
+def calculate_entropy(word, status, possible_words):
+    """
+    (entropy) E[information] = Summation(p(x)*information)
+    This functions calculates the entropy { E[information] } for a given word.
+    """
+    ...
+    
+def calculate_info(word, status, possible_words):
+    """
+    information = -(log2(p(x))) { negative of log base 2 of probability of x }
+    Calculates information { in terms of bits } for a given word.
+    """
+    ...
