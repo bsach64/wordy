a57671623fd89c9fcc148906098d0d6bb1f2dd23
scifre <ayushagent007@gmail.com>
2023-12-13 00:08:07 +0530

    Test solver and test solver multiprocessing

--- src/test_solver_multiprocessing.py
+++ src/test_solver_multiprocessing.py
@@ -1,5 +1,5 @@
 from os import name
-
+import multiprocessing
 from numpy import average
 from wordle import check_word, print_word, EXACT
 from solver import possible_matches, calculate_entropy
@@ -8,18 +8,14 @@ from termcolor import cprint
 from pathlib import Path
 import time
 
-
 file_path_answers = Path("../words/answers/nyt5.txt")
 
 with open(file_path_answers) as file:
-    answers = file.readlines()
-    answers = [word.replace('\n', '') for word in answers]
-
+    answers = [word.replace('\n', '') for word in file.readlines()]
 
 file_path_allowed = Path("../words/answers/nyt5.txt")
 with open(file_path_allowed) as file:
-    words = file.readlines()
-    words = [word.replace('\n', '') for word in words]
+    words = [word.replace('\n', '') for word in file.readlines()]
 
 
 def next_guess(entropies):
@@ -32,34 +28,30 @@ def next_guess(entropies):
     return guess
 
 
-def main():
-    ms = time.time()
-    stats = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
-    help = []
-    times = [] 
-    for index, choice in enumerate(answers):
-        
+def cal(index_range, stats, times, answers, words, lock):
+    for index in range(*index_range):
         start = time.time()
+        choice = answers[index]
         print(f"--{index}--{choice}")
         words_copy = deepcopy(words)
         wordsize = 5
         guesses = wordsize + 1
-        
 
         guess = 'tares'
         previous_guess = set()
         previous_guess.add('tares')
         for i in range(0, guesses):
-
             status = [0] * wordsize
             score = check_word(guess, status, choice)
 
             print(f"Computer Guess {i + 1} : ", end="")
             print_word(guess, status)
             if score == (EXACT * wordsize):
-                stats[i + 1] += 1
-                print(stats)
+                with lock:
+                    stats[i + 1] += 1
+                    print(stats)
                 break
+
             print()
             words_copy = possible_matches(guess, status, words_copy, previous_guess)
             entropies = dict()
@@ -68,25 +60,39 @@ def main():
             guess = next_guess(entropies)
             previous_guess.add(guess)
         stop = time.time()
-        
-        times.append(stop-start)
-        
-        if(index==1000):
-            break
-    print(average(times))
-    #average number of guesses
-    num = 0
-    sums = 0
-    for k in stats.keys():
-        sums += int (k*stats[k])
-        num += int(stats[k])
-    print(sums/num)
-    ma = time.time()
-    print("total time: ", ma-ms)
-        
-            
-            
-        
+        with lock:
+            times.append(stop - start)
 
-if __name__=="__main__":
+
+def main():
+    ms = time.time()
+    with multiprocessing.Manager() as manager:
+        stats = manager.dict({1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0})
+        times = manager.list()
+        lock = manager.Lock()
+
+        index_ranges = [(1, 250), (251, 500), (501, 750), (751, 1000)]
+        processes = []
+
+        for index_range in index_ranges:
+            process = multiprocessing.Process(target=cal, args=(index_range, stats, times, answers, words, lock))
+            processes.append(process)
+            process.start()
+
+        for process in processes:
+            process.join()
+
+        print(average(times))
+        # Average number of guesses
+        num = 0
+        sums = 0
+        for k in stats.keys():
+            sums += int(k * stats[k])
+            num += int(stats[k])
+        print(sums / num)
+    ma = time.time()
+    
+    print("total time: ", ma-ms)
+
+if __name__ == "__main__":
     main()
